0f8c0dc2a403c2e0f47d47c7371348775a0618ba
carlos-angel <c.a.a.a.503@gmail.com>
2022-02-14 11:09:42 -0600

    feat: instalación y configuración de storybook

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+ReactDOM.render(<React.StrictMode></React.StrictMode>, document.getElementById('root'));
